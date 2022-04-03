51c6bc2181caadc6110ab53b75bdbb4ce6367ccc
ziad abo alkamar <ziad-lovelyboy@hotmail.com>
2022-04-03 23:58:51 +0300

    firsat

--- js/main.js
+++ js/main.js
@@ -66,12 +66,13 @@
             var $ltn__utilizeToggle = $('.ltn__utilize-toggle'),
                 $ltn__utilize = $('.ltn__utilize'),
                 $ltn__utilizeOverlay = $('.ltn__utilize-overlay'),
-                $mobileMenuToggle = $('.mobile-menu-toggle');
+                $mobileMenuToggle = $('.mobile-menu-toggle'),
+                $mobileNavBar = $('.ltn__utilize-mobile-menu');
             $ltn__utilizeToggle.on('click', function (e) {
                 e.preventDefault();
                 var $this = $(this),
                     $target = $this.attr('href');
-                $body.addClass('ltn__utilize-open');
+                $mobileNavBar.addClass('ltn__utilize-open');
                 $($target).addClass('ltn__utilize-open');
                 $ltn__utilizeOverlay.fadeIn();
                 if ($this.parent().hasClass('mobile-menu-toggle')) {
